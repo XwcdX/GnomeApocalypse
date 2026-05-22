c2f1efc5e550ac3e4a3b5f49022b5c29c5dd61ad
rickyjooooooe <rjonathan@student.ciputra.ac.id>
2026-05-22 17:58:02 +0700

    refactor: boss health

--- Greed/Config/GameConfig.swift
+++ Greed/Config/GameConfig.swift
@@ -107,7 +107,7 @@ enum GameConfig {
 
     // MARK: - Boss Stage
     /// Interval (in seconds) between Boss eruptions, regardless of Director budget state.
-    static let bossSpawnInterval: TimeInterval = 5.0
+    static let bossSpawnInterval: TimeInterval = 150.0
 
 
     // MARK: - Enemy Spawn
